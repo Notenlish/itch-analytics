fddb8b2bf862fdb99e015133e7c443674c01405d
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-08-23 12:29:20 +0300

    google search console and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { poppins } from "./font";
 import "./globals.css";
 import { TypographyH2 } from "@/ui/typography";
 import Footer from "@/ui/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Itch.io Analytics",
@@ -16,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-08BE4C6F4F" />
+      <head>
+        {/* Google Search Console verification */}
+        <meta
+          name="google-site-verification"
+          content="Gurmrqg74NHiG1yD0S6seTXKXN1R3bL7-aVK38bjBNk"
+        />
+      </head>
       <body className={`min-h-screen bg-background antialiased ${poppins.className}`}>
         <>
           <header className="pt-8">
