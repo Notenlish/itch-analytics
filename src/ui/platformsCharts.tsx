26d919bbd96b6d1b2e55b4e5a0b78e7fa902be10
Notenlish <71970100+Notenlish@users.noreply.github.com>
2024-08-28 12:35:42 +0300

    no label for pie chart

--- src/ui/platformsCharts.tsx
+++ src/ui/platformsCharts.tsx
@@ -123,15 +123,17 @@ export default function PlatformsChart({
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[500px]">
+          className="mx-auto aspect-square max-h-[350px]">
           <PieChart>
             <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
             <Pie data={chartData} dataKey="count" nameKey="platform"></Pie>
+            {/*
             <ChartLegend
               fontSize={16}
               content={<ChartLegendContent nameKey="platform" />}
               className=" w-full -translate-y-2 flex-wrap gap-x-2 gap-y-0 [&>*]:basis-1/4 [&>*]:justify-center"
             />
+            */}
           </PieChart>
         </ChartContainer>
       </CardContent>
