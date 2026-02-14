2c324feb9a99f5c9eab985b3f832027d90f9540a
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-02-14 12:19:53 +0300

    rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const rules = (() => {
     try {
       return JSON.parse(process.env.BLOCK_RULES || "{}");
